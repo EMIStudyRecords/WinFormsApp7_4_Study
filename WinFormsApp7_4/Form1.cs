3a68a143de07b47d27337f281b763345a0b21d49
鈴木 映美 <yon.ncity@gmail.com>
2023-12-13 10:47:04 +0900

    コメント追加

--- WinFormsApp7_4/Form1.cs
+++ WinFormsApp7_4/Form1.cs
@@ -7,11 +7,13 @@ namespace WinFormsApp7_4
             InitializeComponent();
         }
 
+        // 登録ボタン押下時
         private void AddButtonClicked(object sender, EventArgs e)
         {
 
         }
 
+        // 削除ボタン押下時
         private void RemoveButtonClicked(object sender, EventArgs e)
         {
 
